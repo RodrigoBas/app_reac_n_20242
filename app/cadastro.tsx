4f4093c4c2cc7911032259c7a511553674f643df
RodrigoBas <149122688+RodrigoBas@users.noreply.github.com>
2024-09-14 00:21:11 +0000

    Tudo

---
+++ app/cadastro.tsx
@@ -0,0 +1,8 @@
+import CadastroProduto from "./Componentes/Adaptadores/CadastroProdutos";
+
+export default function Cadastro(){
+    return (
+        <CadastroProduto></CadastroProduto>
+    );
+
+}
